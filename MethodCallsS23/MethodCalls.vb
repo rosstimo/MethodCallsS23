55a03dd9f43af5dd28cd54b147ee22def3a33d75
tim <rosstimo@isu.edu>
2023-01-23 07:16:15 -0800

    Add project files.

---
+++ MethodCallsS23/MethodCalls.vb
@@ -0,0 +1,14 @@
+'Tim Rossiter
+'RCET0265
+'MethodCalls
+
+Option Strict On
+Option Explicit On
+
+Imports System
+
+Module MethodCalls
+    Sub Main(args As String())
+        'Console.WriteLine("Hello World!")
+    End Sub
+End Module
